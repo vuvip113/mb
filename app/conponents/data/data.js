9ca7aef86acd8e376bc5a0458d7bc88b4c5de23b
Vuzuize <vucute775@gmail.com>
2022-10-01 12:57:27 +0700

    ko

---
+++ app/conponents/data/data.js
@@ -0,0 +1,14 @@
+export const sliderData = [
+  {
+    title: 'First game',
+    img : require('../../assets/banner/1.jpg')
+  },
+  {
+    title: 'First game',
+    img : require('../../assets/banner/2.jpg')
+  },
+  {
+    title: 'First game',
+    img : require('../../assets/banner/3.jpg')
+  }
+]
--- app/src/Home/Home.js
+++ app/src/Home/Home.js
@@ -1,9 +1,10 @@
-import { View, Text, ScrollView, ImageBackground, TextInput } from 'react-native'
-import React from 'react';
+import { View, Text, ScrollView, ImageBackground, TextInput, Touchable, TouchableOpacity, FlatList, Image } from 'react-native'
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useFonts } from 'expo-font'
 import CustomInput from '../../conponents/CustomInput/CustomInput'
 import { AntDesign } from '@expo/vector-icons';
+import sliderData from '../../conponents/data/data'
 
 
 const Home = () => {
@@ -13,17 +14,27 @@ const Home = () => {
   })
   if (!fontsLoaded) return null;
 
+  const [categories, setCategories] = useState(sliderData);
+
   return (
     <SafeAreaView style={{ flex: 1 }} >
       <ScrollView style={{ padding: 20 }}>
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20 }}>
-          <Text style={{ fontSize: 20, fontFamily: 'Vare', }}>Hello ADMIN</Text>
-          <ImageBackground source={require('../../assets/icon/admin.png')} style={{ width: 35, height: 35 }} imageStyle={{ borderRadius: 25 }} />
+          <Text style={{ fontSize: 20, fontFamily: 'Vare', }}>Hello Vu Tran</Text>
+          <ImageBackground source={require('../../assets/icon/icon2.png')} style={{ width: 40, height: 40 }} imageStyle={{ borderRadius: 25 }} />
         </View>
-        <View style={{flexDirection:'row',borderColor:'#c6c6c6',borderWidth:1,borderRadius:5}}>
-          <AntDesign name="search1" size={17} color="#c6c6c6" style={{margin:5}}/>
-          <TextInput placeholder='Search'/>
+        <View style={{ flexDirection: 'row', borderColor: '#c6c6c6', borderWidth: 1, borderRadius: 5 }}>
+          <AntDesign name="search1" size={17} color="#c6c6c6" style={{ margin: 5 }} />
+          <TextInput placeholder='Search' />
         </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20, marginVertical: 15 }}>
+          <Text style={{ fontSize: 20, fontFamily: 'Vare', }}>Upcoming Games</Text>
+          <TouchableOpacity onPress={() => { }}>
+            <Text style={{ color: '#0aada8' }}>See all</Text>
+          </TouchableOpacity>
+        </View>
+
+
       </ScrollView>
     </SafeAreaView>
   )
